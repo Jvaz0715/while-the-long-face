78390df767cf3bd53c23b7527ca472e199b8eac2
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-04 14:20:15 -0500

    Solves numberDoubler function

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 function numberDoubler(num) {
     // Your code here
+    //start output num at num
+    let outputNum = num;
+    // while outputNum is less than 100, loop
+    while (outputNum < 100) {
+        console.log('outputNum: ' + outputNum);
+        outputNum = outputNum * 2;
+    }
+    // return output num
+    console.log('outputnum: ' + outputNum);
+    return outputNum;
 }
+
 
 function stringRepeater(str) {
     // Your code here
+    let outputString = str;
+    while (outputString.length < 10) {
+        outputString = outputString + 1;
+    }
+    
+    return outputString;
+
 }
 
 function makeDivisible(x, y) {
